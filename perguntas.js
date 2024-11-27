a9a068c8a17e92b62a22952031abef7170403a75
SandroDuarte <00001084455092sp@al.educacao.sp.gov.br>
2024-11-27 11:22:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Anime',
+    'Qual o nome do Protagonista?',
+    'Gon'
+)
+
+criaCartao(
+    'Anime',
+    'Qual a técnica de Nen de Gon?',
+    'Jajanken'
+)
+
+criaCartao(
+    'Anime',
+    'Quem ensinou o Nen para o Gon e o Killua?',
+    'Wing'
+)
+
+criaCartao(
+    'Anime',
+    'Qual é o nome do Clã dos assasinos onde o Killua participa?',
+    'Clã Zoldyck'
+)
